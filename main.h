b4faabbfa1395b0dcd47e184a80f3b3806dcf99b
Mona2Mohamed <mona.memo@fci.suezuni.edu.eg>
2023-05-30 10:43:31 -0700

    {}

--- main.h
+++ main.h
@@ -1,7 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+#define NULL_STRING "(null)"
+
+
 int _putchar(char c);
+
+/* _printf.c module */
 int _printf(const char *format, ...);
+
+/* print_function.c module */
+
 int formatPrint(const char *format, va_list input);
 #endif
