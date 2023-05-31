b32818454d6073f87cc4c4aaa15205f83809aa5f
Mona2Mohamed <mona.memo@fci.suezuni.edu.eg>
2023-05-31 13:51:19 -0700

    edit putchar

--- putcharFunction.c
+++ putcharFunction.c
@@ -9,30 +9,5 @@
  */
 int _putchar(char c)
 {
-	static int x;
-	static char buf[OUTPUT_BUF_SIZE];
-
-	if (c == BUF_FLUSH || x >= OUTPUT_BUF_SIZE)
-	{
-		write(1, buf, x);
-		x = 0;
-	}
-	if (c !=BUF_FLUSH)
-		buf[x++] = c;
-	return (1);
-}
-
-/**
- * _put - prints a string with  newline
- * @str: the string to print
- *
- * Return : void
- */
-int _puts(char *str)
-{
-	char *p =str;
-
-	while (*str)
-		_putchar(*str++);
-	return (str - p);
+	return (write(1, &c, 1));
 }
